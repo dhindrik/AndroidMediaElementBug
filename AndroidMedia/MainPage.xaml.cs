93bd0051198e233d2a88cc6455b27b573e36d7ca
Daniel Hindrikes <daniel@hindrikes.se>
2025-02-03 09:41:29 +0100

    Updated sample

--- AndroidMedia/MainPage.xaml.cs
+++ AndroidMedia/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using CommunityToolkit.Maui.Views;
+using System.Windows.Input;
 
 namespace AndroidMedia
 {
@@ -13,6 +14,7 @@ namespace AndroidMedia
 
         public bool ShowPlayer { get; set; }
         public string VideoFilePath { get; set; }
+        public MediaSource VideoSource => MediaSource.FromFile(VideoFilePath);
 
         public ICommand PickVideoCommand => new Command(async () =>
         {
@@ -29,12 +31,14 @@ namespace AndroidMedia
 
         private async Task HandleVideoResult(FileResult? fileResult)
         {
-            if (fileResult == null)
+            if (fileResult is null)
+            {
                 return;
+            }
 
-            var tempFileName = $"temp_meps_video{Path.GetExtension(fileResult.FileName)}";
+            var tempFileName = $"temp_meps_video{Path.GetExtension(fileResult?.FileName)}";
 
-            var stream = await fileResult.OpenReadAsync();
+            var stream = await fileResult?.OpenReadAsync();
             var memoryStream = new MemoryStream();
             await stream.CopyToAsync(memoryStream);
 
@@ -44,13 +48,7 @@ namespace AndroidMedia
 
             stream.Close();
             memoryStream.Close();
-
-            VideoFilePath = $"@{localPath}";
-            ShowPlayer = true;
-
-            OnPropertyChanged(nameof(VideoFilePath));
-            OnPropertyChanged(nameof(ShowPlayer));
-
+            mediaElement.Source = MediaSource.FromFile(localPath);
         }
     }
 
